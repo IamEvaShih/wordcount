36cb136548baca17c813f2c5824363ab7fd0c0e2
JJC <jjc@nlplab.cc>
2017-03-08 22:35:10 +0800

    new filter function

--- wordcount.py
+++ wordcount.py
@@ -5,12 +5,24 @@ import string
 from nltk.corpus import stopwords
 
 stopwords = set(stopwords.words('english'))
+stopwords.update(string.punctuation)
+
+
+def word_is_valid(word):
+    if word in stopwords:
+        return False
+    if word.isalnum():
+        return True
+    if all(char in string.punctuation for char in word):
+        return False
+    return True
+
 
 if __name__ == '__main__':
     wordCounter = Counter()
     for line in fileinput.input():
         words = word_tokenize(line.strip())
-        wordCounter.update(word for word in words if word not in string.punctuation and word not in stopwords)
+        wordCounter.update(filter(word_is_valid, words))
 
     for word, count in wordCounter.most_common(20):
         print("%s: %d" % (word, count))
