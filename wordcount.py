f8a5d9f697e571696273e08f9454c53dac8053a6
JJC <jjc@nlplab.cc>
2017-03-08 22:27:10 +0800

    filter symbols

--- wordcount.py
+++ wordcount.py
@@ -1,12 +1,13 @@
 import fileinput
 from collections import Counter
-from nltk import word_tokenize, pos_tag
+from nltk import word_tokenize
+import string
 
 if __name__ == '__main__':
     wordCounter = Counter()
     for line in fileinput.input():
         words = word_tokenize(line.strip())
-        wordCounter.update(words)
+        wordCounter.update(word for word in words if word not in string.punctuation)
 
     for word, count in wordCounter.most_common(20):
         print("%s: %d" % (word, count))
